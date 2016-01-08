e075130e4ec10aaba070eaf8499514e6de079938
GeneralCarter <jcarter.jc8@gmail.com>
2016-01-08 08:35:19 -0500

    Add Ravenhome Project

---
+++ Test/Ravenhome.Test/Services/RavenDBServiceTest.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Ravenhome.Services;
+using Xunit;
+
+namespace Ravenhome.Test.Services
+{
+    public class RavenDBServiceTest
+    {
+        private readonly RavenDBService _ravenDBService;
+
+        public RavenDBServiceTest()
+        {
+            _ravenDBService = new RavenDBService();
+        }
+
+        [Fact]
+        public void Init()
+        {
+            var result = _ravenDBService.Init();
+
+            Assert.True(true, String.Format("Initilization failed"));
+        }
+
+        [Fact]
+        public void GetSession()
+        {
+            _ravenDBService.Init();
+            using (var session = _ravenDBService.getSession())
+            {
+                Assert.NotNull(session);
+            }
+        }
+
+    }
+}
---
+++ src/Ravenhome/Services/RavenDBService.cs
@@ -0,0 +1,27 @@
+﻿using Raven.Client;
+using Raven.Client.Document;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Ravenhome.Services
+{
+    public class RavenDBService : IRavenDBService
+    {
+        private IDocumentStore _store;
+
+        public Task Init()
+        {
+            _store = new DocumentStore { Url = "http://carter-box:8088" };
+            _store.Initialize();
+
+            return Task.FromResult(true);
+        }
+
+        public IDocumentSession getSession()
+        {
+            return _store.OpenSession();
+        }
+    }
+}
